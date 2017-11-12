15d9a19bc847f4a331c0a223a8c113ea68623b35
joeyzia <joey.zia@gmail.com>
2017-11-12 10:22:43 -0500

    Test Run

--- retreat/prices_inc.php
+++ retreat/prices_inc.php
@@ -3,15 +3,15 @@
 date_default_timezone_set("America/New_York");
 
 function prices_get_cost_and_expl(&$explanation, &$expl_ascii, $school = 'SCHOOL')
-{  
+{
   // prices and dates
-  $EARLY_DATE = 20171218;
-  $EARLY_PRICE = 90;
+  $EARLY_DATE = 20171112;
+  $EARLY_PRICE = 95;
 
-  $NORMAL_DATE = 20180116;
-  $NORMAL_PRICE = 105;
+  $NORMAL_DATE = 20180114;
+  $NORMAL_PRICE = 110;
 
-  $LATE_PRICE = 120;
+  $LATE_PRICE = 125;
 
   $myDate = date("Ymd");
   if ($myDate < $EARLY_DATE) {
@@ -28,11 +28,11 @@ function prices_get_cost_and_expl(&$explanation, &$expl_ascii, $school = 'SCHOOL
   $explanation = <<<EOF
   <div style="border: 1px #009900 solid; width:400px">
     <table cellpadding='4' align='center'>
-      <tr><td>$period Price: </td><td align=\"right\"> \$$price</td></tr> 
+      <tr><td>$period Price: </td><td align=\"right\"> \$$price</td></tr>
     </table>
   </div>
 EOF;
-  
+
   $expl_ascii  = "    +------------------------------+------+\n";
   $expl_ascii .= "    | $period               | " .($price<100?" ":""). "\$$price |\n";
   $expl_ascii .= "    +------------------------------+------+\n";
