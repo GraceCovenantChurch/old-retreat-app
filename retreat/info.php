913f8ae57086ca214551bbc3ddf84fd7096de75a
joeyzia <joey.zia@gmail.com>
2017-11-26 15:05:17 -0500

    Updated info

--- retreat/info.php
+++ retreat/info.php
@@ -10,7 +10,7 @@ echo_tabs();
     <ul>
     <li>Mainline:
       <ul>
-	<li>Contact Pastor Mike for pickup locations and times! <a href="mailto: mikekim37@gmail.com">mikekim37@gmail.com</a></li>
+	<li>Contact Pastor Josh for pickup locations and times! <a href="mailto: josh.kwon@gmail.com">josh.kwon@gmail.com</a></li>
         <!--<li>Bryn Mawr: 4:45pm at Baldwin School</li>
         <li>Eastern: 4:20pm at McInnis and HHC Parking Lot</li>
         <li>Haverford: 4:40pm at South Lot</li>
@@ -22,17 +22,17 @@ echo_tabs();
     </ul>
   </li>
   <!--li><b>Theme:</b> </li-->
-  <li><b>Speaker:</b> TBA</li>
+  <li><b>Speaker:</b> Pastor Eddie Kim</li>
   <li><b>Pricing:</b>
     <ul>
-      <li>Early Registration: $90 | December 18th</li>
-      <li>Regular Registration: $105 | December 19th - January 15th</li>
-      <li>Late Registration: $120 | After January 16th</li>
+      <li>Early Registration: $95 | December 24th</li>
+      <li>Regular Registration: $110 | December 25th - January 13th</li>
+      <li>Late Registration: $125 | After January 14th</li>
       <li><i>Financial aid is available - please email <span class="email"><a href="mailto:retreatstaff@gracecovenant.net">retreatstaff@gracecovenant.net</a></span></i></li>
     </ul>
   </li>
   <li>Please email <span class="email"><a href="mailto:retreatstaff@gracecovenant.net">retreatstaff@gracecovenant.net</a></span> with any questions or concerns regarding the retreat!</li>
-</ul>	  
+</ul>
 
  <!--center><img src="/images/slides/college_retreat_2017.jpg"/></center--><br>
 
